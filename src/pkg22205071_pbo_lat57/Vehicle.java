4f314fd7a06732bccd4d17222d7aa65dbb8c5305
farchatul07 <145920038+farchatul07@users.noreply.github.com>
2023-11-29 16:24:42 +0700

    Update Vehicle.java

--- src/pkg22205071_pbo_lat57/Vehicle.java
+++ src/pkg22205071_pbo_lat57/Vehicle.java
@@ -7,6 +7,11 @@ package pkg22205071_pbo_lat57;
 /**
  *
  * @author user
+ Nama              : Farchatul Hudayah
+ Nim               : 22205071
+ Prodi             : Teknik Informatika
+ Kelas             : PBO1
+ Deskripsi Program : untuk menampilkan hasil VEHICLE
  */
 public class Vehicle {
       private String myBrand;
